db8b39da57089f59985133238a0a3ed5b47595fc
SirBlobman <blobmansrealm@gmail.com>
2021-03-31 13:16:18 -0400

    5.0.1: Update for SirBlobmanCore language features.

--- src/main/java/com/github/sirblobman/disco/armor/DiscoArmorPlugin.java
+++ src/main/java/com/github/sirblobman/disco/armor/DiscoArmorPlugin.java
@@ -5,13 +5,10 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.sirblobman.api.configuration.ConfigurationManager;
-import com.github.sirblobman.api.configuration.PlayerDataManager;
-import com.github.sirblobman.api.core.CorePlugin;
+import com.github.sirblobman.api.core.plugin.ConfigurablePlugin;
 import com.github.sirblobman.api.language.LanguageManager;
-import com.github.sirblobman.api.nms.MultiVersionHandler;
 import com.github.sirblobman.api.update.UpdateChecker;
 import com.github.sirblobman.api.utility.MessageUtility;
 import com.github.sirblobman.api.utility.VersionUtility;
@@ -27,16 +24,11 @@ import com.github.sirblobman.disco.armor.pattern.SmoothPattern;
 import com.github.sirblobman.disco.armor.pattern.YellowOrangePattern;
 import com.github.sirblobman.disco.armor.task.DiscoArmorTask;
 
-public class DiscoArmorPlugin extends JavaPlugin {
-    private final ConfigurationManager configurationManager;
-    private final LanguageManager languageManager;
-    private final PlayerDataManager playerDataManager;
+public class DiscoArmorPlugin extends ConfigurablePlugin {
     private final PatternManager patternManager;
     private DiscoArmorTask discoArmorTask;
+
     public DiscoArmorPlugin() {
-        this.configurationManager = new ConfigurationManager(this);
-        this.languageManager = new LanguageManager(this, this.configurationManager);
-        this.playerDataManager = new PlayerDataManager(this);
         this.patternManager = new PatternManager(this);
         this.discoArmorTask = new DiscoArmorTask(this);
     }
@@ -46,7 +38,9 @@ public class DiscoArmorPlugin extends JavaPlugin {
         ConfigurationManager configurationManager = getConfigurationManager();
         configurationManager.saveDefault("config.yml");
         configurationManager.saveDefault("language.yml");
-        configurationManager.saveDefault("language/en_us.lang.yml");
+
+        LanguageManager languageManager = getLanguageManager();
+        languageManager.saveDefaultLocales();
     }
 
     @Override
@@ -58,87 +52,39 @@ public class DiscoArmorPlugin extends JavaPlugin {
             logger.warning("You should not be using it on " + VersionUtility.getMinecraftVersion());
         }
 
-        PatternManager patternManager = getPatternManager();
-        patternManager.register(GrayscalePattern.class);
-        patternManager.register(OldGloryPattern.class);
-        patternManager.register(OneColorPattern.class);
-        patternManager.register(RainbowPattern.class);
-        patternManager.register(RandomPattern.class);
-        patternManager.register(SmoothPattern.class);
-        patternManager.register(YellowOrangePattern.class);
+        registerPatterns();
+        registerCommands();
+        registerListeners();
+        registerTasks();
+
+        UpdateChecker updateChecker = new UpdateChecker(this, 60700L);
+        updateChecker.runCheck();
 
         ConfigurationManager configurationManager = getConfigurationManager();
         YamlConfiguration configuration = configurationManager.get("config.yml");
-        long taskPeriod = configuration.getLong("armor-speed");
-
-        CommandDiscoArmor command = new CommandDiscoArmor(this);
-        command.register();
-
-        PluginManager manager = Bukkit.getPluginManager();
-        manager.registerEvents(new ListenerDiscoArmor(this), this);
-
-        DiscoArmorTask task = getDiscoArmorTask();
-        task.runTaskTimer(this, 5L, taskPeriod);
-
         String enableBroadcast = configuration.getString("broadcast.enabled");
         if(enableBroadcast != null) {
             String message = MessageUtility.color(enableBroadcast);
             Bukkit.broadcastMessage(message);
         }
-
-        UpdateChecker updateChecker = new UpdateChecker(this, 60700L);
-        updateChecker.runCheck();
     }
 
     @Override
     public void onDisable() {
         DiscoArmorTask discoArmorTask = getDiscoArmorTask();
-        discoArmorTask.cancel();
-        discoArmorTask.disableAll();
+        if(discoArmorTask != null) {
+            discoArmorTask.cancel();
+            discoArmorTask.disableAll();
+        }
 
         ConfigurationManager configurationManager = getConfigurationManager();
         YamlConfiguration configuration = configurationManager.get("config.yml");
         String disableBroadcast = configuration.getString("broadcast.disabled");
+
         if(disableBroadcast != null) {
             String message = MessageUtility.color(disableBroadcast);
             Bukkit.broadcastMessage(message);
         }
-    }
-
-    @Override
-    public YamlConfiguration getConfig() {
-        ConfigurationManager configurationManager = getConfigurationManager();
-        return configurationManager.get("config.yml");
-    }
-
-    @Override
-    public void reloadConfig() {
-        DiscoArmorTask discoArmorTask = getDiscoArmorTask();
-        discoArmorTask.cancel();
-        discoArmorTask.disableAll();
-
-        ConfigurationManager configurationManager = getConfigurationManager();
-        configurationManager.reload("config.yml");
-        configurationManager.reload("language.yml");
-        configurationManager.reload("language/en_us.lang.yml");
-
-        YamlConfiguration configuration = configurationManager.get("config.yml");
-        long taskPeriod = configuration.getLong("armor-speed");
-
-        this.discoArmorTask = new DiscoArmorTask(this);
-        this.discoArmorTask.runTaskTimer(this, 5L, taskPeriod);
-    }
-
-    public ConfigurationManager getConfigurationManager() {
-        return this.configurationManager;
-    }
-
-    public LanguageManager getLanguageManager() {
-        return this.languageManager;
-    }
-
-    public PlayerDataManager getPlayerDataManager() {
-        return this.playerDataManager;
     }
 
     public PatternManager getPatternManager() {
@@ -149,8 +95,49 @@ public class DiscoArmorPlugin extends JavaPlugin {
         return this.discoArmorTask;
     }
 
-    public MultiVersionHandler getMultiVersionHandler() {
-        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
-        return corePlugin.getMultiVersionHandler();
+    public void onReload() {
+        ConfigurationManager configurationManager = getConfigurationManager();
+        configurationManager.reload("config.yml");
+        configurationManager.reload("language.yml");
+
+        LanguageManager languageManager = getLanguageManager();
+        languageManager.reloadLocales();
+
+        registerTasks();
+    }
+
+    private void registerPatterns() {
+        PatternManager patternManager = getPatternManager();
+        patternManager.register(GrayscalePattern.class);
+        patternManager.register(OldGloryPattern.class);
+        patternManager.register(OneColorPattern.class);
+        patternManager.register(RainbowPattern.class);
+        patternManager.register(RandomPattern.class);
+        patternManager.register(SmoothPattern.class);
+        patternManager.register(YellowOrangePattern.class);
+    }
+
+    private void registerCommands() {
+        new CommandDiscoArmor(this).register();
+    }
+
+    private void registerListeners() {
+        PluginManager manager = Bukkit.getPluginManager();
+        manager.registerEvents(new ListenerDiscoArmor(this), this);
+    }
+
+    private void registerTasks() {
+        DiscoArmorTask discoArmorTask = getDiscoArmorTask();
+        if(discoArmorTask != null) {
+            discoArmorTask.cancel();
+            discoArmorTask.disableAll();
+        }
+
+        ConfigurationManager configurationManager = getConfigurationManager();
+        YamlConfiguration configuration = configurationManager.get("config.yml");
+        long taskPeriod = configuration.getLong("armor-speed");
+
+        this.discoArmorTask = new DiscoArmorTask(this);
+        this.discoArmorTask.runTaskTimer(this, 5L, taskPeriod);
     }
 }
